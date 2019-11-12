dd08a6adee5bb99a016d934dc360a2dda7e89b29
sixer51 <you@example.com>
2019-11-12 22:46:51 +0000

    launch file works! :D

--- src/treasure-retriever/scripts/pose_to_tf.py
+++ src/treasure-retriever/scripts/pose_to_tf.py
@@ -6,29 +6,30 @@ import rospy
 from geometry_msgs.msg import Vector3, PoseStamped, Quaternion, Transform
 from tf import TransformBroadcaster
 
-def pose_callback(msg, tf_broadcaster):
-
-    pose = msg.pose
-
-    q = Quaternion()
-    q.x = pose.orientation.x
-    q.y = pose.orientation.y
-    q.z = pose.orientation.z
-    q.w = pose.orientation.w
-
-    tf_broadcaster = TransformBroadcaster()
-    tf_broadcaster.sendTransform(
-        translation=Vector3(pose.position.x, pose.position.y, 0.0),
-        rotation=q,
-        time=rospy.Time(), 
-        child="odom",
-        parent="base_link")
-
 def main():
     rospy.init_node("pose_to_tf")
-    tf_broadcaster = tf_broadcaster = TransformBroadcaster()
-    rospy.Subscriber("/pose", PoseStamped, pose_callback, queue_size=1, callback_args=tf_broadcaster)
+
+    tf_broadcaster = TransformBroadcaster()
+
+    def pose_callback(msg):
+
+        pose = msg.pose
+        q = pose.orientation
+        # q = Quaternion()
+        # q.x = pose.orientation.x
+        # q.y = pose.orientation.y
+        # q.z = pose.orientation.z
+        # q.w = pose.orientation.w
+
+        tf_broadcaster.sendTransform(
+            translation=(pose.position.x, pose.position.y, 0),
+            rotation=(q.x, q.y, q.z, q.w),
+            time=rospy.Time.now(), 
+            child="odom",
+            parent="base_link")
+
+    rospy.Subscriber("/pose", PoseStamped, pose_callback, queue_size=1)
     rospy.spin()
-    
+
 if __name__ == "__main__":
     main()
